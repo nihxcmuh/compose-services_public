720f610a7bfcbf2648732b3c57f192904d2e692a
Ubuntu <summerhill001@gmail.com>
2022-12-29 22:42:23 +0800

    iframe

---
+++ patch/scripts/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './page.css';
+
+
+class DocumentPage extends React.Component {
+  render() {
+    return (
+<div class="container">
+  <iframe class="responsive-iframe" src="/lamp"></iframe>
+</div>
+   );
+  }
+}
+
+
+export default DocumentPage;
